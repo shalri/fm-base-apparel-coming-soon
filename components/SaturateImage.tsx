2be77ac386130517fd26d535e77b0a82769e48ee
shalri <fsaonlinework777@gmail.com>
2024-07-30 21:27:00 +0800

    Add conditional for window

--- components/SaturateImage.tsx
+++ components/SaturateImage.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { m, useMotionValue, useTransform } from "framer-motion";
+import { type } from "os";
 import { useEffect, useState } from "react";
 
 interface SaturateImageProps {
@@ -15,22 +16,27 @@ export default function SaturateImage({
 }: SaturateImageProps) {
   const x = useMotionValue(0);
   const saturation = useTransform(x, [-window.innerWidth / 2, window.innerWidth / 2], [20, 100]);
-
+  // const [saturation, setSaturation] = useState(null);
   const [saturationValue, setSaturationValue] = useState(100);
 
   useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      x.set(e.clientX - window.innerWidth / 2);
-    };
+    if (typeof window !== 'undefined') {
+      const handleMouseMove = (e: MouseEvent) => {
+        x.set(e.clientX - window.innerWidth / 2);
+      };
 
-    document.addEventListener('mousemove', handleMouseMove);
-    const unsubscribe = saturation.onChange(value => setSaturationValue(value));
+      document.addEventListener('mousemove', handleMouseMove);
+      const unsubscribe = saturation.onChange((value) => setSaturationValue(value));
 
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      unsubscribe();
-    };
-  }, [saturation, x]);
+      return () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        unsubscribe();
+      };
+    } else {
+      return
+    }
+  }, [x, saturation]);
+
 
   return (
     <m.div
